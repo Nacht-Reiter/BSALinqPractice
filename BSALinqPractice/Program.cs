292e1900ee4f85d5f4fafbd4a21acbb61a4c813d
Nacht-Reiter <maxkolpak@ukr.net>
2018-07-03 14:51:52 +0300

    Enterties

---
+++ BSALinqPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BSALinqPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync("https://5b128555d50a5c0014ef1204.mockapi.io/comments").GetAwaiter().GetResult(); ;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                HttpContent responseContent = response.Content;
+                var json = responseContent.ReadAsStringAsync().GetAwaiter().GetResult(); ;
+                Console.Write(json);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ BSALinqPractice/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BSALinqPractice
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+
+    }
+}
